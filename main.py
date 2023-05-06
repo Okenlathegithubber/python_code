d1e9013d380280fa4d0fcf6a12abf106039e79b8
David Okenla <116645288+Okenlathegithubber@users.noreply.github.com>
2023-05-06 18:55:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-# Making a Dictionary of subjects and Credit unit.
+total_credit_unit = 28
+
+#  course titles and credit unit
 subject_cu = {
     "MTH-101": 3.5,
     "CSC-101": 3.5,
@@ -7,11 +9,24 @@ subject_cu = {
     "PHY-103": 3.5,
     "STA-105": 3.5,
     "GST-103": 3.5,
-    "GST-105": 3.5,
+    "GST-105": 3.5
 }
 
-# Making a Dictionary to compare the letter grade and grade point.
-letter_grade_to_grade_point = {
+
+# course titles and corresponding scores
+courses = {
+    "MTH-101": int(input("Insert MTH-101 score >> ")),
+    "CSC-101": int(input("Insert CSC-101 score >> ")),
+    "CHM-101": int(input("Insert CHM-101 score >> ")),
+    "PHY-101": int(input("Insert PHY-101 score >> ")),
+    "PHY-103": int(input("Insert PHY-103 score >> ")),
+    "STA-105": int(input("Insert STA-105 score >> ")),
+    "GST-103": int(input("Insert GST-103 score >> ")),
+    "GST-105": int(input("Insert GST-105 score >> "))
+}
+
+# dictionary of letter grades and their corresponding weight points
+grades = {
     "A": 4.00,
     "AB": 3.50,
     "B": 3.25,
@@ -23,29 +38,57 @@ letter_grade_to_grade_point = {
     "F": 0.00
 }
 
-# Seperating the keys (courses) from the values(credit unit).
-for course, credit_unit in subject_cu.items():
-    score = int(input(f"Enter score of {course}: "))
+# create a table to store subject, score, letter grade and unit of each course
+table = []
+total_unit = 0
 
-    # determine the corresponding letter grade for the score
+# loop through the courses and calculate the unit of each subject/course
+for course, score in courses.items():
     if score >= 75:
-        letter_grade = "A"
+        grade = "A"
     elif score >= 70:
-        letter_grade = "AB"
+        grade = "AB"
     elif score >= 65:
-        letter_grade = "B"
+        grade = "B"
     elif score >= 60:
-        letter_grade = "BC"
+        grade = "BC"
     elif score >= 55:
-        letter_grade = "C"
+        grade = "C"
     elif score >= 50:
-        letter_grade = "CD"
+        grade = "CD"
     elif score >= 45:
-        letter_grade = "D"
+        grade = "D"
     elif score >= 40:
-        letter_grade = "E"
+        grade = "E"
     else:
-        letter_grade = "F"
-    
-    # To display the course, examination score, and letter_grade.    
-    print(f"{course}: {score} {letter_grade}")
+        grade = "F"
+    weight_point = grades[grade]
+    unit = weight_point * total_credit_unit / len(courses)
+    total_unit += unit
+    table.append((course, score, grade, round(unit, 2)))
+
+    # calculate the grade point average and round to 2 decimal places
+    gpa = total_unit / total_credit_unit
+    gpa = round(gpa, 2)
+
+    # print the course table
+    print("{:<15}{:<15}{:<15}{:<15}".format("Subject", "Score", "Letter Grade", "Unit"))
+    for row in table:
+        print("{:<15}{:<15}{:<15}{:<15}".format(row[0], row[1], row[2], row[3]))
+
+    # determine the range of the grade point average
+    if gpa >= 3.50:
+        range = "Distinction"
+    elif gpa >= 3.00:
+        range = "Upper Credit"
+    elif gpa >= 2.50:
+        range = "Lower Credit"
+    elif gpa >= 2.00:
+        range = "Pass"
+    else:
+        range = "Fail"
+
+    # print the grade point average and the range
+    print("\nGrade Point Average: {}".format(gpa))
+    print("Range: {}".format(range))
+
